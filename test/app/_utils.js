8f0c2be662d1d375d327e77bb5c73660ebd2afcc
Yun Lai <lyonlai1984@gmail.com>
2017-03-18 02:12:07 +1100

    rename the store generator to the default one.

--- test/app/_utils.js
+++ test/app/_utils.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import {runGeneratorWithPrompts} from '../_utils';
 
-export const storePath = path.join(__dirname, '../../generators/store');
+export const storePath = path.join(__dirname, '../../generators/app');
 
 export function runStoreWithPrompt(prompts) {
   return runGeneratorWithPrompts(storePath, prompts);
